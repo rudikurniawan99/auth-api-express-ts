d6d3fa5f6055bc69c761b439567cd1739b6f379c
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-01-05 02:26:36 +0700

    create user.router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Response, Router } from 'express'
+import userRouter from './user.routes'
 
 const router = Router()
 
 router.get('/test', (_, res: Response) => res.sendStatus(200))
+router.use('/api/users', userRouter)
 
 export default router
