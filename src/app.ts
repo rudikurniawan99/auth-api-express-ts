0aaac9d76fa3b52a3a3d0899431ffc0097979c8e
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-01-05 01:16:53 +0700

    setup config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('hello')
+require('dotenv').config()
+import express from 'express'
+import config from 'config'
+
+const app = express()
+
+const port = config.get('port')
+
+app.listen(port, () => {
+  console.log(`app is running in http://localhost:${port}`)
+})
