4df035940824f4957208f7f9d7417213d6450f05
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-01-07 16:49:37 +0700

    create user.model

---
+++ src/models/user.model.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose'
+import argon2 from 'argon2'
+
+interface UserDocument extends mongoose.Document{
+  firstname: string
+  lastname: string
+  email: string
+  password: string
+}
+
+const userSchema = new mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+},{
+  timestamps: true
+})
+
+userSchema.pre('save', async function(next) {
+  let user = this as UserDocument
+  if(!user.isModified('password')){
+    next()
+  }
+
+  const hash = await argon2.hash(user.password)
+  user.password = hash
+  next()
+})
+
+userSchema.methods.verifyPassword = async function(inputPassword: string): Promise<boolean>{
+  const user = this as UserDocument
+  try {
+    return await argon2.verify(user.password, inputPassword)
+  } catch (e) {
+    return false 
+  }
+
+}
+
+const User = mongoose.model<UserDocument>('User', userSchema)
+
+export default User
