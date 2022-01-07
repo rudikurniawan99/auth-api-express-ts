179e7541a11049ec4a40786bd5c1e93a2c9d33b2
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-01-07 22:25:17 +0700

    minor update

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,15 +1,23 @@
 import { Request, Response } from "express"
 import { CreateUserInput } from "../schema/user.schema"
-import { createUser } from "../services/user.services"
+import { createUser, findByEmail } from "../services/user.services"
 
 export const createUserHandler = async (req: Request<{},{}, CreateUserInput>, res: Response) => {
   const { body } = req 
 
   try {
+
+    const checkEmail = await findByEmail(body.email)
+    if(checkEmail){
+      res.status(402).json({
+        error: true,
+        message: 'email already exist'
+      })
+    }
+
     const user = await createUser(body)  
     res.status(201).json({
-      message: 'succes to create data',
-      ...user
+      firstname: user.firstname,
     })
   } catch (e: any) {
     throw new Error(e) 
--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -3,3 +3,9 @@ import User, { UserDocument } from "../models/user.model";
 export const createUser = (input: Partial<UserDocument>) => {
   return User.create(input)
 }
+
+export const findByEmail = (email: string) => {
+  return User.findOne({
+    email
+  })
+}
